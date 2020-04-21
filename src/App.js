af52173fb15fa39697ed98a888aa162a86cea4a4
Ludde86 <bslbjorn@gmail.com>
2020-04-21 11:04:55 +0200

    add SeachBar.js

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import React, { useEffect } from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import M from 'materialize-css/dist/js/materialize.min.js';
 import './App.css';
+import SearchBar from './components/layout/SearchBar';
 
 const App = () => {
 	useEffect(() => {
 		// init materialize JS
 		M.AutoInit();
 	});
-	return <div className="App">My App</div>;
+	return (
+		<div className="App">
+			<SearchBar />
+		</div>
+	);
 };
 
 export default App;
